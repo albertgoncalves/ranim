39b1261921d550e6fc4d8a8c4980a6b8f5454705
albertgoncalves <goncalves.albert@gmail.com>
2020-01-03 22:01:26 -0500

    hmm segmentation fault

--- dev/src/growth.rs
+++ dev/src/growth.rs
@@ -1,5 +1,12 @@
 mod point;
 
+use glutin_window::GlutinWindow;
+use graphics::math::Matrix2d;
+use graphics::Transformed;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent};
+use piston::window::WindowSettings;
 use point::Point;
 use rand::rngs::ThreadRng;
 use rand::Rng;
@@ -11,17 +18,22 @@ const WINDOW_HEIGHT: f64 = 600.0;
 const WINDOW_HALF_WIDTH: f64 = WINDOW_WIDTH / 2.0;
 const WINDOW_HALF_HEIGHT: f64 = WINDOW_HEIGHT / 2.0;
 
+const LIGHT_GRAY: [f32; 4] = [0.95, 0.95, 0.95, 1.0];
+const DARK_GRAY: [f32; 4] = [0.15, 0.15, 0.15, 1.0];
+
+const LINE_WIDTH: f64 = 0.7;
+
 const PI_2: f64 = f64::consts::PI * 2.0;
 
 const INIT: usize = 5;
-const SPREAD: f64 = 10.0;
+const SPREAD: f64 = 100.0;
 
 const SEARCH_RADIUS: f64 = 10.0;
 const SEARCH_RADIUS_SQUARED: f64 = SEARCH_RADIUS * SEARCH_RADIUS;
 
 const GATE: f32 = 0.1;
 const THRESHOLD: f64 = 10.0;
-const DRAG_ATTRACT: f64 = 5.0;
+const DRAG_ATTRACT: f64 = 10.0;
 const DRAG_REJECT: f64 = 10.0;
 
 #[derive(Debug)]
@@ -128,89 +140,6 @@ fn build_tree<'a>(
     }))
 }
 
-fn init(rng: &mut ThreadRng) -> Vec<Node> {
-    let mut nodes: Vec<Node> = Vec::with_capacity(INIT);
-    for _ in 0..INIT {
-        let angle: f64 = rng.gen_range(0.0, PI_2);
-        nodes.push(Node {
-            point: Point {
-                x: (angle.cos() * SPREAD) + WINDOW_HALF_WIDTH,
-                y: (angle.sin() * SPREAD) + WINDOW_HALF_HEIGHT,
-            },
-            next: empty_point!(),
-            left: ptr::null_mut(),
-            right: ptr::null_mut(),
-            neighbors: Vec::with_capacity(INIT),
-        });
-    }
-    let n: usize = INIT - 1;
-    for i in 0..INIT {
-        let (l, r): (usize, usize) = {
-            if i == 0 {
-                (n, 1)
-            } else if i == n {
-                (i - 1, 0)
-            } else {
-                (i - 1, i + 1)
-            }
-        };
-        nodes[i].left = &mut nodes[l];
-        nodes[i].right = &mut nodes[r];
-    }
-    nodes
-}
-
-fn insert(rng: &mut ThreadRng, nodes: &mut Vec<Node>) {
-    let mut selection: Option<*mut Node> = None;
-    unsafe {
-        for node in nodes.iter_mut() {
-            if (rng.gen::<f32>() < GATE)
-                && (THRESHOLD
-                    < point::squared_distance(
-                        &node.point,
-                        &(*node.left).point,
-                    ))
-            {
-                selection = Some(node);
-                break;
-            }
-        }
-        if let Some(node) = selection {
-            nodes.push(Node {
-                point: Point {
-                    x: ((*(*node).left).point.x + (*node).point.x) / 2.0,
-                    y: ((*(*node).left).point.y + (*node).point.y) / 2.0,
-                },
-                next: empty_point!(),
-                left: (*node).left,
-                right: node,
-                neighbors: Vec::with_capacity(INIT),
-            });
-            let new_node: *mut Node = nodes.last_mut().unwrap();
-            (*(*node).left).right = new_node;
-            (*node).left = new_node;
-        }
-    }
-}
-
-fn neighbors_within_radius<'a, 'b, 'c>(
-    point: &'a Point,
-    points: &'b mut Vec<&'a Point>,
-    tree: &'c Option<Box<Tree<'a>>>,
-) {
-    if let Some(tree) = tree {
-        let x: f64 = point.x
-            - tree.bounds.lower.x.max(point.x.min(tree.bounds.upper.x));
-        let y: f64 = point.y
-            - tree.bounds.lower.y.max(point.y.min(tree.bounds.upper.y));
-        if ((x * x) + (y * y)) < SEARCH_RADIUS_SQUARED {
-            points.push(tree.point);
-            neighbors_within_radius(point, points, &tree.left);
-            neighbors_within_radius(point, points, &tree.right);
-        }
-    }
-}
-
 fn transform<'a>(nodes: &'a [Node<'a>]) -> Option<Box<Tree<'a>>> {
     let n: usize = nodes.len();
     let mut points: Vec<&Point> = Vec::with_capacity(n);
@@ -233,6 +162,24 @@ fn transform<'a>(nodes: &'a [Node<'a>]) -> Option<Box<Tree<'a>>> {
             },
         },
     )
+}
+
+fn neighbors_within_radius<'a, 'b, 'c>(
+    point: &'a Point,
+    points: &'b mut Vec<&'a Point>,
+    tree: &'c Option<Box<Tree<'a>>>,
+) {
+    if let Some(tree) = tree {
+        let x: f64 = point.x
+            - tree.bounds.lower.x.max(point.x.min(tree.bounds.upper.x));
+        let y: f64 = point.y
+            - tree.bounds.lower.y.max(point.y.min(tree.bounds.upper.y));
+        if ((x * x) + (y * y)) < SEARCH_RADIUS_SQUARED {
+            points.push(tree.point);
+            neighbors_within_radius(point, points, &tree.left);
+            neighbors_within_radius(point, points, &tree.right);
+        }
+    }
 }
 
 fn update_neighbors<'a>(
@@ -274,8 +221,116 @@ fn update_positions(nodes: &mut [Node]) {
     }
 }
 
+fn insert(rng: &mut ThreadRng, nodes: &mut Vec<Node>) {
+    let mut selection: Option<*mut Node> = None;
+    unsafe {
+        for node in nodes.iter_mut() {
+            if (rng.gen::<f32>() < GATE)
+                && (THRESHOLD
+                    < point::squared_distance(
+                        &node.point,
+                        &(*node.left).point,
+                    ))
+            {
+                selection = Some(node);
+                break;
+            }
+        }
+        if let Some(node) = selection {
+            nodes.push(Node {
+                point: Point {
+                    x: ((*(*node).left).point.x + (*node).point.x) / 2.0,
+                    y: ((*(*node).left).point.y + (*node).point.y) / 2.0,
+                },
+                next: empty_point!(),
+                left: (*node).left,
+                right: node,
+                neighbors: Vec::with_capacity(INIT),
+            });
+            let new_node: *mut Node = nodes.last_mut().unwrap();
+            (*(*node).left).right = new_node;
+            (*node).left = new_node;
+        }
+    }
+}
+
+fn render(gl: &mut GlGraphics, args: &RenderArgs, nodes: &[Node]) {
+    unsafe {
+        gl.draw(args.viewport(), |context, gl| {
+            let transform: Matrix2d = context
+                .transform
+                .trans(args.window_size[0] / 2.0, args.window_size[1] / 2.0);
+            graphics::clear(DARK_GRAY, gl);
+            for node in nodes {
+                graphics::line(
+                    LIGHT_GRAY,
+                    LINE_WIDTH,
+                    [
+                        (*node.left).point.x,
+                        (*node.left).point.y,
+                        node.point.x,
+                        node.point.y
+                    ],
+                    transform,
+                    gl,
+                )
+            }
+        })
+    }
+}
+
 fn main() {
+    let opengl: OpenGL = OpenGL::V3_2;
+    let mut window: GlutinWindow =
+        WindowSettings::new("ranim", [WINDOW_WIDTH, WINDOW_HEIGHT])
+            .graphics_api(opengl)
+            .exit_on_esc(true)
+            .build()
+            .unwrap();
+    let mut events: Events = Events::new(EventSettings::new());
+    let mut gl: GlGraphics = GlGraphics::new(opengl);
     let mut rng: ThreadRng = rand::thread_rng();
-    let nodes: Vec<Node> = init(&mut rng);
-    println!("{:#?}", nodes);
+    let mut nodes: Vec<Node> = Vec::with_capacity(INIT);
+    let mut angles: Vec<f64> = Vec::with_capacity(INIT);
+    for _ in 0..INIT {
+        angles.push(rng.gen_range(0.0, PI_2));
+    }
+    angles.sort_by(|a, b| a.partial_cmp(&b).unwrap());
+    for i in 0..INIT {
+        nodes.push(Node {
+            point: Point {
+                x: (angles[i].cos() * SPREAD),
+                y: (angles[i].sin() * SPREAD),
+            },
+            next: empty_point!(),
+            left: ptr::null_mut(),
+            right: ptr::null_mut(),
+            neighbors: Vec::with_capacity(INIT),
+        });
+    }
+    let n: usize = INIT - 1;
+    for i in 0..INIT {
+        let (l, r): (usize, usize) = {
+            if i == 0 {
+                (n, 1)
+            } else if i == n {
+                (i - 1, 0)
+            } else {
+                (i - 1, i + 1)
+            }
+        };
+        nodes[i].left = &mut nodes[l];
+        nodes[i].right = &mut nodes[r];
+    }
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            {
+                render(&mut gl, &args, &nodes);
+            }
+            {
+                let tree: Option<Box<Tree>> = transform(&nodes);
+                update_neighbors(&mut nodes, &tree);
+            }
+        }
+    }
 }
